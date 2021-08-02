fbb21f07a6d1ec35be620166fefce4f4788b629b
kym1924 <kym1924@naver.com>
2021-08-02 18:48:28 +0900

    #15 [layout] NotificationFragment

---
+++ app/src/main/java/how/about/it/view/notification/NotificationFragment.kt
@@ -0,0 +1,35 @@
+package how.about.it.view.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import how.about.it.databinding.FragmentNotificationBinding
+
+class NotificationFragment : Fragment() {
+    private var _binding: FragmentNotificationBinding? = null
+    private val binding get() = requireNotNull(_binding)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        setNotificationBackClickListener()
+        return binding.root
+    }
+
+    private fun setNotificationBackClickListener() {
+        binding.btnNotificationBack.setOnClickListener {
+            requireView().findNavController()
+                .popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
