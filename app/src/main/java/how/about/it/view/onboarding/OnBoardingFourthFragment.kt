2633f02e1dbb125b5ae15425bfc39996e2d2c151
kym1924 <kym1924@naver.com>
2021-08-09 20:22:50 +0900

    #46 [layout] OnBoarding

---
+++ app/src/main/java/how/about/it/view/onboarding/OnBoardingFourthFragment.kt
@@ -0,0 +1,26 @@
+package how.about.it.view.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import how.about.it.databinding.FragmentOnBoardingFourthBinding
+
+class OnBoardingFourthFragment : Fragment() {
+    private var _binding: FragmentOnBoardingFourthBinding? = null
+    private val binding get() = requireNotNull(_binding)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnBoardingFourthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
