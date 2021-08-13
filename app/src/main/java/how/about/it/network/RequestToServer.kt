64f922a03886736af2687d0ebe1a0978cfdb8f16
DongJun-H <dongjun826@seoultech.ac.kr>
2021-08-14 04:05:23 +0900

    #3 [feature] 이메일 로그인 기능 구현
    issue: #3

--- app/src/main/java/how/about/it/network/RequestToServer.kt
+++ app/src/main/java/how/about/it/network/RequestToServer.kt
@@ -3,7 +3,6 @@ package how.about.it.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 object RequestToServer {
     var retrofit = Retrofit.Builder()
         .baseUrl("http://ec2-3-34-111-109.ap-northeast-2.compute.amazonaws.com/")
