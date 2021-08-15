8e1a3c4691e55372ca66b440ae1f198e4268bb60
kym1924 <kym1924@naver.com>
2021-08-15 20:28:25 +0900

    #79 [feature] Setting FeedAdapter

---
+++ app/src/main/java/how/about/it/view/feed/adapter/FeedBottomAdapter.kt
@@ -0,0 +1,35 @@
+package how.about.it.view.feed.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import how.about.it.databinding.ItemFeedBottomBinding
+import how.about.it.view.feed.Feed
+
+class FeedBottomAdapter(private val feedDiffUtil: DiffUtil.ItemCallback<Feed>) :
+    ListAdapter<Feed, FeedBottomAdapter.FeedBottomViewHolder>(feedDiffUtil) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ) = FeedBottomViewHolder(
+        ItemFeedBottomBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
+
+    override fun onBindViewHolder(holder: FeedBottomViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class FeedBottomViewHolder(
+        private val binding: ItemFeedBottomBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(feed: Feed) {
+
+        }
+    }
+}
