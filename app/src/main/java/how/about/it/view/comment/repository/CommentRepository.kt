86894ec60dffb1b82d498f60e55e452256a871a3
kym1924 <kym1924@naver.com>
2021-08-19 14:22:53 +0900

    #134 [feature] Update Comment

--- app/src/main/java/how/about/it/view/comment/repository/CommentRepository.kt
+++ app/src/main/java/how/about/it/view/comment/repository/CommentRepository.kt
@@ -1,7 +1,11 @@
 package how.about.it.view.comment.repository
 
 import how.about.it.network.comment.CommentServiceImpl
+import how.about.it.view.commentupdate.RequestPutComment
 
 class CommentRepository(private val commentServiceImpl: CommentServiceImpl) {
     suspend fun requestCommentReply(id: Int) = commentServiceImpl.requestCommentReply(id)
+
+    suspend fun requestCommentUpdate(body: RequestPutComment) =
+        commentServiceImpl.requestCommentUpdate(body)
 }
