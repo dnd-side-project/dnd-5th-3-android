fec7fb28fda6d1d2524503d54f78c4c69b71717f
DongJun-H <dongjun826@seoultech.ac.kr>
2021-08-20 00:56:57 +0900

    #154 [layout] Layout Delete Account in SettingFragment
    resolved: #154

--- app/src/main/java/how/about/it/view/setting/SettingFragment.kt
+++ app/src/main/java/how/about/it/view/setting/SettingFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import how.about.it.R
@@ -47,6 +48,13 @@ class SettingFragment : Fragment() {
         showBackButton()
     }
 
+    private fun deleteSharedPreferencesInfo() {
+        val prefs: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val editor = prefs.edit()
+        editor.clear().commit()
+    }
+
     private fun setLogoutClickListener() {
         binding.tvSettingLogout.setOnClickListener {
             val mDialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_default_confirm, null)
@@ -65,11 +73,7 @@ class SettingFragment : Fragment() {
                     mAlertDialog.dismiss()
                 }
                 confirmButton.setOnClickListener {
-                    val prefs: SharedPreferences =
-                        PreferenceManager.getDefaultSharedPreferences(requireContext())
-                    val editor = prefs.edit()
-                    editor.clear().commit()
-
+                    deleteSharedPreferencesInfo()
                     val loginIntent = Intent(activity, LoginActivity::class.java)
                     startActivity(loginIntent)
                     (activity as MainActivity).finish()
@@ -82,7 +86,41 @@ class SettingFragment : Fragment() {
 
     private fun setDeleteAccountClickListener() {
         binding.tvSettingDeleteAccount.setOnClickListener {
+            val mDialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_default_confirm, null)
+            val mBuilder = AlertDialog.Builder(requireContext()).setView(mDialogView)
+            val mAlertDialog = mBuilder.create()
+            // Dialog 중복 실행 방지
+            if(mAlertDialog != null && !mAlertDialog.isShowing){
+                mAlertDialog.show()
 
+                mDialogView.findViewById<TextView>(R.id.tv_message_dialog_title).setText(R.string.setting_delete_account)
+                mDialogView.findViewById<TextView>(R.id.tv_message_dialog_description).setText(R.string.setting_delete_account_check_dialog_description)
+
+                val confirmButton = mDialogView.findViewById<Button>(R.id.btn_dialog_confirm)
+                val cancelButton = mDialogView.findViewById<Button>(R.id.btn_dialog_cancel)
+                cancelButton.setOnClickListener {
+                    mAlertDialog.dismiss()
+                }
+                confirmButton.setOnClickListener {
+                    /** TODO: 서버와 통신하여 회원정보 삭제 코드 작성 **/
+                    mAlertDialog.dismiss()
+                    mDialogView.findViewById<TextView>(R.id.tv_message_dialog_title).setText(R.string.setting_delete_account_complete)
+                    mDialogView.findViewById<TextView>(R.id.tv_message_dialog_description).setText(R.string.setting_delete_account_complete_dialog_description)
+                    mDialogView.findViewById<ConstraintLayout>(R.id.layout_dialog_cancel).visibility = View.GONE
+                    mAlertDialog.setCancelable(false)
+                    mAlertDialog.show()
+                    deleteSharedPreferencesInfo()
+
+                    // 메인 로그인 화면으로 돌아가도록 설정
+                    val deleteAccountConfirmButton = mDialogView.findViewById<Button>(R.id.btn_dialog_confirm)
+                     deleteAccountConfirmButton.setOnClickListener {
+                        val loginIntent = Intent(activity, LoginActivity::class.java)
+                        startActivity(loginIntent)
+                        (activity as MainActivity).finish()
+                    }
+                }
+            }
+            deleteSharedPreferencesInfo()
         }
     }
 
