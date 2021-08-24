43158afa3585213d7ef6c26aa91dcd72e791274c
DongJun-H <dongjun826@seoultech.ac.kr>
2021-08-24 16:26:48 +0900

    #177 [layout] Create Notice Fragment in SettingFragment
    resolved: #177

---
+++ app/src/main/java/how/about/it/view/notice/NoticePostFragment.kt
@@ -0,0 +1,43 @@
+package how.about.it.view.notice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import how.about.it.R
+import how.about.it.databinding.FragmentNoticePostBinding
+import how.about.it.view.main.MainActivity
+
+class NoticePostFragment : Fragment() {
+
+    private var _binding: FragmentNoticePostBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNoticePostBinding.inflate(layoutInflater)
+        setToolbarDetail()
+
+        return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        (activity as MainActivity).onBackPressed()
+        return true
+    }
+    private fun showBackButton() {
+        (activity as MainActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        this.setHasOptionsMenu(true)
+    }
+    private fun setToolbarDetail() {
+        binding.toolbarNoticePostBoard.tvToolbarTitle.setText(R.string.notice)
+        (activity as MainActivity).setSupportActionBar(binding.toolbarNoticePostBoard.toolbarBoard)
+        (activity as MainActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        showBackButton()
+    }
+}
