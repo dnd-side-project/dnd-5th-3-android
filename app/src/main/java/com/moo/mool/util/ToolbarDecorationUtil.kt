000b6089c839b77050732b4a4a6b631907974811
DongJun-H <dongjun826@seoultech.ac.kr>
2021-09-27 17:23:53 +0900

    #369 [refactoring] LoginFragment
    resolved: #369

--- app/src/main/java/com/moo/mool/util/ToolbarDecorationUtil.kt
+++ app/src/main/java/com/moo/mool/util/ToolbarDecorationUtil.kt
@@ -1,16 +1,16 @@
 package com.moo.mool.util
 
 import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.moo.mool.R
 import com.moo.mool.databinding.ActivityToolbarBinding
-import com.moo.mool.view.main.MainActivity
 
 object ToolbarDecorationUtil {
     fun setToolbarDetail(toolbarBoard : ActivityToolbarBinding, titleStringResId: Int, baseActivity : Activity, baseFragment: Fragment) {
         // setTitle
         toolbarBoard.tvToolbarTitle.setText(titleStringResId)
-        (baseActivity as MainActivity).setSupportActionBar(toolbarBoard.toolbarBoard)
+        (baseActivity as AppCompatActivity).setSupportActionBar(toolbarBoard.toolbarBoard)
         baseActivity.supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // showBackButton
